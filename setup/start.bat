92e67d3fdee9ae8f20c71698304b899c006123be
Christoph Cernusca <cernusca.c22@htlwienwest.at>
2026-03-23 07:25:53 +0100

    Venv parent path fix

--- setup/start.bat
+++ setup/start.bat
@@ -11,17 +11,17 @@ if exist "%ROBUS_CORE%\venv\Scripts\activate.bat" (
     echo Activating venv: %ROBUS_CORE%\env
     call "%ROBUS_CORE%\env\Scripts\activate.bat"
 ) else if exist "%PARENT%\venv\Scripts\activate.bat" (
-    echo Activating venv: %PARENT%\venv
-    call "%PARENT%\venv\Scripts\activate.bat"
+    echo Activating venv: %PARENT%venv
+    call "%PARENT%venv\Scripts\activate.bat"
 ) else if exist "%PARENT%\env\Scripts\activate.bat" (
-    echo Activating venv: %PARENT%\env
-    call "%PARENT%\env\Scripts\activate.bat"
+    echo Activating venv: %PARENT%env
+    call "%PARENT%env\Scripts\activate.bat"
 ) else (
     echo No virtual environment found in:
     echo   %ROBUS_CORE%\venv
     echo   %ROBUS_CORE%\env
-    echo   %PARENT%\venv
-    echo   %PARENT%\env
+    echo   %PARENT%venv
+    echo   %PARENT%env
     echo Using system Python.
 )
 
